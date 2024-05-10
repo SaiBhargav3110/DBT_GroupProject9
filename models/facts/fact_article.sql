9dcc7ba46184dee5fe1df37db620f54cb559e8c9
SaiBhargav Podili <kumar.podili@sjsu.edu>
2024-05-10 21:39:36 +0000

    fact article

---
+++ models/facts/fact_article.sql
@@ -0,0 +1,28 @@
+select
+    _id,
+    abstract,
+    web_url,
+    lead_paragraph,
+    source,
+    print_section,
+    print_page,
+    headline.main,
+    to_hex(cast(md5(headline.kicker) as bytes)) as article_type_id,
+    to_hex(cast(md5(m.url) as bytes)) as image_url_id,
+    to_hex(cast(md5(k.name || k.value || k.rank) as bytes)) as keyword_id,
+    pub_date,
+    document_type,
+    news_desk,
+    section_name,
+    subsection_name,
+    byline.original,
+    to_hex(
+        cast(md5(bp.firstname || bp.middlename || bp.lastname) as bytes)
+    ) as author_id,
+    type_of_material,
+    word_count
+from
+    `crack-will-422608-j1.GroupProject.article`,
+    unnest(multimedia) m,
+    unnest(keywords) k,
+    unnest(byline.person) bp
