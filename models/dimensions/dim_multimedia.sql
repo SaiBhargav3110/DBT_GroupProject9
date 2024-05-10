255059bb5c1d5a42ddbef26b40d37376a779f065
SaiBhargav Podili <kumar.podili@sjsu.edu>
2024-05-10 20:38:11 +0000

    configured them as tables

--- models/dimensions/dim_multimedia.sql
+++ models/dimensions/dim_multimedia.sql
@@ -1,12 +1,11 @@
---select multimedia from  `crack-will-422608-j1.GroupProject.test` limit 15
-
-
-
---multimedia table
-select To_HEX(CAST(MD5(url) AS BYTES)) as url_id,subtype,type, Url,height,width,crop_name FROM
-`crack-will-422608-j1.GroupProject.article`,  UNNEST(multimedia) AS multimedia 
-
-
-
-
-
+-- select multimedia from  `crack-will-422608-j1.GroupProject.test` limit 15
+-- multimedia table
+select
+    to_hex(cast(md5(url) as bytes)) as url_id,
+    subtype,
+    type,
+    url,
+    height,
+    width,
+    crop_name
+from `crack-will-422608-j1.GroupProject.article`, unnest(multimedia) as multimedia
