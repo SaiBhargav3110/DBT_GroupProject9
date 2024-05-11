1e600938fdb0747850cec8e792305cd8c80d8d46
sindhura p <nagasaisindhura.pandrangi@sjsu.edu>
2024-05-11 00:18:26 +0000

    updated

--- models/dimensions/dim_multimedia.sql
+++ models/dimensions/dim_multimedia.sql
@@ -1,7 +1,7 @@
 -- select multimedia from  `crack-will-422608-j1.GroupProject.test` limit 15
 -- multimedia table
 select
-    to_hex(cast(md5(url) as bytes)) as url_id,
+    Distinct to_hex(cast(md5(url) as bytes)) as url_id,
     subtype,
     type,
     url,
